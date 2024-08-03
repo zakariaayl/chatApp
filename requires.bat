3dc1abea9483ec17131f5384ff4cfffa9b91b24e
zakariaayl <ayougilzaki@gmail.com>
2024-08-03 21:13:27 +0200

    Update requires.bat

--- requires.bat
+++ requires.bat
@@ -1,15 +1,12 @@
-
-pip install Flask
-pip install Flask-SocketIO
-pip install SQLAlchemy
-pip install deep-translator
-pip install mtranslate
-pip install gunicorn
-pip install Pillow
-pip install itsdangerous
-pip install python-dotenv
-pip install Flask-Mail
-pip install pymysql
-pip install flask_sqlalchemy
-
-python app.py
+Flask
+Flask-SocketIO
+SQLAlchemy
+deep-translator
+mtranslate
+gunicorn
+Pillow
+itsdangerous
+python-dotenv
+Flask-Mail
+pymysql
+flask_sqlalchemy
